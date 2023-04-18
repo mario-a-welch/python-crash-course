45af199101cd676830b64a20b98493c4ce7de8da
mario welch <mario.welch01@gmail.com>
2023-04-18 14:38:03 -0500

    simple message variables

---
+++ simple_messages.py
@@ -0,0 +1,11 @@
+# Assign a message to a variable, and then print that message
+
+test_message = "Testing..1..2..3"
+print(test_message)
+
+# Assign a message to a variable, and print that message.  Then change the value of the variable to a new message, and
+# print the new message
+
+# test_message_two = "Second test"
+test_message_two = "Second test of message two"
+print(test_message_two)
