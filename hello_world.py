1b43dc452a89b67c2a098ebedf7216443a7c7a69
mario welch <mario.welch01@gmail.com>
2023-04-18 14:32:39 -0500

    variables

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,15 @@
 print("Hello Python world!")
 
+# Variable Rules
+# 1. Variable names can contain only letters, numbers, and underscores.  They can start with a letter or an underscore,
+# but not with a number.  Ex: you can call a variable message_1 but not 1_message
+# 2. Spaces are not allowed in variable names, but underscores can be used to separate words in variable names
+# 3. Avoid using Python keywords and function names as variable names. Ex: print as a variable name
+# 4. Variable names should be short but descriptive
+# 5. Be careful using the lowercase letter l and uppercase letter O, they can be confused with 1 and 0
+
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
