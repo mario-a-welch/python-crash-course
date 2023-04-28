b65f90368c9c7e422fb24680618c0c0ff35d1532
mario welch <mario.welch01@gmail.com>
2023-04-28 16:32:02 -0500

    numbers and zen of python

---
+++ numbers.py
@@ -0,0 +1,27 @@
+# Multiple Assignment
+x, y, z = 0, 0, 0
+# initialize all variables to 0
+
+MAX_CONNECTIONS = 5000
+# A constant is a variable whose value stays the same throughout the life of a program.  Python programmers use all
+# capital letters to indicate a variable should be treated as a constant and never be changed.
+
+print("-----")
+
+# Number Eight - Write addition, subtraction, multiplication, and division operations that each result in the number 8
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(16 / 2)
+
+# Favorite Number - Use a variable to represent your favorite number. Then, using that variable, create a message that
+# reveals your favorite number
+
+fav_num = 8
+message = f"My favorite number is {fav_num}"
+
+print(message)
+
+print("-----")
+
+import this
